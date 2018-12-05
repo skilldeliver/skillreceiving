5d9b2b5ec5f5db68543113f4b38e53d1b5fe03d9
Vladislav Mihov <37806520+skilldeliver@users.noreply.github.com>
2018-12-05 08:30:32 +0200

    Update utils.py

--- Advent of Code/utils.py
+++ Advent of Code/utils.py
@@ -1,6 +1,9 @@
-def load_input(link):
+def load_input(link, as_string=False):
     import urllib.request
 
     uf = urllib.request.urlopen(link)
     content = uf.readlines()
+
+	if as_string:
+		return content.replace('\r\n', '')
     return [line.decode('utf-8').strip('\r\n') for line in content]
