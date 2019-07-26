eba65bb288e44f15b546da38a3d960a78bc6b07f
Vladislav Mihov <37806520+skilldeliver@users.noreply.github.com>
2019-07-26 18:55:34 +0300

    c++ kata

---
+++ Codewars/C++/7 kyu Reverse words.h
@@ -0,0 +1,30 @@
+#include <string>
+#include <iostream>
+
+std::string reverse_words(std::string str)
+{
+    std::string result = "";
+    int start = 0;
+
+    for (int i = 0; i < str.size(); i++) {
+        if (i == (str.size() - 1) && str[i] != ' ')
+        {
+            for (int j = i; j >= start; j--) {
+                result += str[j];
+            }
+        }
+        else if (str[i] == ' ')
+        {
+            start = i + 1;
+            result += ' ';
+        }
+        else if (str[i + 1] == ' ' and str[i] != ' ')
+        {
+            for (int j = i; j >= start; j--) {
+                result += str[j];
+            }
+        }
+
+    }
+    return result;
+}
