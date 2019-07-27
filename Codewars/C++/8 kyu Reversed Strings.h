acece2dd34aa4fd3a2034a4d612096ec0320bf25
Vladislav Mihov <37806520+skilldeliver@users.noreply.github.com>
2019-07-27 23:24:15 +0300

    3 8 kyu's

---
+++ Codewars/C++/8 kyu Reversed Strings.h
@@ -0,0 +1,10 @@
+#include <string>
+#include <algorithm>
+
+using namespace std ;
+
+string reverseString (string str)
+{
+  reverse(str.begin(), str.end());
+  return str;
+}
---
+++ Codewars/C++/8 kyu get character from ASCII Value.h
@@ -0,0 +1,3 @@
+char get_char(int i) {
+ return (char) i;
+}
