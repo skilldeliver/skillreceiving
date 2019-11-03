0f4d59aaf8daf8cfe04e5571455c01c8ea8c0fc6
Vladislav Mihov <37806520+skilldeliver@users.noreply.github.com>
2019-11-03 23:21:21 +0200

    Programming C++ Homework 3 - Exercise 1

---
+++ NBU/Homework 03/CSCB112_HW_3_F96045_EX_1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string roman(int num) {
+    string result;
+    
+    string letters[] = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+    int nums[] = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+    int i = sizeof(nums)/sizeof(*nums) - 1;
+
+    while (num > 0) {
+        int times = num / nums[i];
+        num = num % nums[i];
+
+        for (int j = 0; j < times; ++j) {
+            result += letters[i];	
+        }
+        i--;
+    }
+    return result;
+}
