cb3f8f7a7d5bc0ba548bc23349513bfea5267677
Vladislav Mihov <vlad.slav.vo.wo@gmail.com>
2020-10-29 17:53:20 +0200

    Single number - Leetcode problem.

---
+++ LeetCode/005_single_number.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = nums[0]
+
+        for i in nums[1:]:
+            n = n ^ i
+            print(nums[i], n)
+
+        return n
+
+
+s = Solution()
+print(s.singleNumber([1,3,1,-1,3]))
