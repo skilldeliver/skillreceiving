d64045cd412899ead585df53c6dde08ae5e7dedf
Vladislav Mihov <vlad.slav.vo.wo@gmail.com>
2020-11-02 14:42:40 +0200

    Reverse string (fix) - LeetCode problem.

--- LeetCode/Strings/001_reverse_string.py
+++ LeetCode/Strings/001_reverse_string.py
@@ -1,7 +1,12 @@
+# https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/879/
+
 class Solution(object):
     def reverseString(self, s):
         """
         :type s: List[str]
         :rtype: None Do not return anything, modify s in-place instead.
         """
-        s = s[::-1]
+        t = list()
+        for _ in range(len(s)):
+            t.append(s.pop())
+        s += t
