46523e78371d995d73cc102e6bb2124dd30bdfe5
Vladislav Mihov <vlad.slav.vo.wo@gmail.com>
2020-11-07 16:16:20 +0200

    Valid palindrome - LeetCdoe problem

---
+++ LeetCode/Strings/005_valid_palindrome.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/883/
+
+from string import ascii_letters, digits
+
+class Solution:
+	def isPalindrome(self, s: str) -> bool:
+		alphanumeric_chars = ascii_letters + digits
+
+		filtered_s = str()
+
+		for c in s:
+			if c in alphanumeric_chars:
+				filtered_s += c 
+		return filtered_s.lower() == filtered_s[::-1].lower()        
