def34b0d4632691aa559e711edc13a7b03827aa6
Vladislav Mihov <vlad.slav.vo.wo@gmail.com>
2020-11-21 19:28:56 +0200

    This is fucking crazy - I can't solve it cmon bro!

--- LeetCode/Trees/002_validate_binary_search_tree.py
+++ LeetCode/Trees/002_validate_binary_search_tree.py
@@ -1,38 +1,43 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 class Solution:
-    def isValidBST(self, root: TreeNode) -> bool:
+    passed = False
+
+    def searchTree(self, root: TreeNode) -> bool:
         if root == None:
             return True
 
         if root.left == root.right == None:
-            return True
-
+            if not Solution.passed:
+                return True
+            else:
+                return root.val
+        elif not Solution.passed:
+            Solution.passed = True
         left = right = None
+
         if root.left:
-            left = Solution.isValidBST(self, root.left)
-
-        if left is False:
-            return False
-
-        if left is not None and root.left.val >= root.val:
-            return False
+            left = Solution.searchTree(self, root.left)
 
         if root.right:
-            right = Solution.isValidBST(self, root.right)
+            right = Solution.searchTree(self, root.right)
 
-        if right is False:
-            return False
+        if type(left) is list:
+            return [*left, root.val, *right]#, left < root.val < right
+        else:
+            return [left, root.val, right]
 
-        if right is not None and root.right.val <= root.val:
-            return False
-        return True
+    def isValidBST(self, root: TreeNode) -> bool:
+        l = Solution.searchTree(self, root)
+        l = [i for i in l if i is not None]
+        return sorted(l) == l
 
-t = TreeNode(val=5, left=TreeNode(val=1), right=TreeNode(val=4, left=TreeNode(3), right=TreeNode(6)))
+r = TreeNode(val=2, left=TreeNode(val=1), right=TreeNode(val=3))
+t = TreeNode(val=5, left=TreeNode(val=1, left=TreeNode(val=-1), right=TreeNode(val=3)), right=TreeNode(val=7, left=TreeNode(6), right=TreeNode(8)))
 s = Solution()
 print(s.isValidBST(t))
