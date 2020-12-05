fa0cd80f9d0a15a04972dc1a509be88de2d00b50
Vladislav Mihov <vlad.slav.vo.wo@gmail.com>
2020-12-05 15:44:28 +0200

    Advent of code - day 5.

---
+++ Advent of Code/2020/day05.py
@@ -0,0 +1,41 @@
+
+def part_one(data, find_max=True):
+    max_seat = 0
+    seats = list()
+
+    lower = lambda x: range(x.start, x.start+(len(x)//2))
+    upper = lambda x: range(x.start+len(x)//2, x.stop)
+
+    for seat in data:
+        all_seats = (range(128), range(8))
+
+        for char in seat:
+            if char == 'F':
+                row = lower(row) 
+            elif char == 'B':
+                row = upper(row)
+            elif char == 'L':
+                column = lower(column)
+            elif char == 'R':
+                column = upper(column)
+
+        current = row.start*8+column.start
+
+        if find_max:
+            if current > max_seat:
+                max_seat = current
+        else:
+            seats.append(current)
+    return max_seat if find_max else seats
+
+
+def part_two(data):
+    seats = sorted(part_one(data, find_max=False))
+    for i, j in zip(seats, seats[1:]):
+        if j - i != 1:
+            return i + 1
+
+with open('inputs/day05.txt') as f:
+    data = [line for line in f.readlines()]
+    print(part_one(data))
+    print(part_two(data))
